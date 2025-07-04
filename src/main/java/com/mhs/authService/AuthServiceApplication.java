cfaff6d4b4547e31827e6c8c12fe9eebce7adfa1
Milad <miladhaghighatshahedi@gmail.com>
2025-07-04 20:07:43 +0330

    9th commit: AuthServiceApplication.java class annotated with @EnableConfigurationProperties(RsaKeyProperties.class)
    this annotation is used to enable support for configuration properties, It works @ConfigurationProperties which is used to bind external configuration (from a properties or YAML file) to a POJO (Plain Old Java Object).

--- src/main/java/com/mhs/authService/AuthServiceApplication.java
+++ src/main/java/com/mhs/authService/AuthServiceApplication.java
@@ -1,8 +1,11 @@
 package com.mhs.authService;
 
+import com.mhs.authService.token.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class AuthServiceApplication {
 
