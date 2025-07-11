bbd28d47c5a67d7ea15aee759c70a9f48ad80cdd
Milad <miladhaghighatshahedi@gmail.com>
2025-07-11 19:20:10 +0330

    35th commit: The project is up and running with no error
    Until this part of the project the main objective meaning methods of AuthenticationController including register , login , rotate and logout work correctly with no error.
    
    1_ A defaultUserSetup.java class which works as the user , role , permission initializer added in a package called setup. since this class implements ApplicationRunner it does not access to HttpServletRequest at the time of running. I have two options here, one to change the signature of the Register method and remove the HttpServletRequest from it or mock the HttpServletRequest in the setup class, I chose the dumbest option meaning mock the HttpServletRequest "may God forgive me!". hopefully-LOL
    
    2_ Also for checking the endpoints I created a file called authentication.http to check each endpoint one by one.

---
+++ src/main/java/com/mhs/authService/setup/defaultUserSetup.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2025-2026 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mhs.authService.setup;
+
+import com.mhs.authService.authentication.AuthenticationService;
+import com.mhs.authService.authentication.dto.AuthenticationRequest;
+import com.mhs.authService.iam.permission.PermissionService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Milad Haghighat Shahedi
+ */
+
+//@Component
+@AllArgsConstructor
+public class defaultUserSetup implements ApplicationRunner {
+
+	private final PermissionService permissionService;
+	private final AuthenticationService authenticationService;
+
+	@Override
+	@Transactional
+	public void run(ApplicationArguments args){
+		createPermissionsAndRoles();
+		createUsers();
+	}
+
+	private void createPermissionAndRoleIfNotExists(String permissionName,String roleName){
+		permissionService.findByRoleNameAndPermissionIfNotExistsCreate(roleName, permissionName);
+	}
+
+	private void createPermissionsAndRoles(){
+		createPermissionAndRoleIfNotExists("PERMIT_ADD_PRODUCT", "ROLE_USER");
+		createPermissionAndRoleIfNotExists("PERMIT_UPDATE_PRODUCT", "ROLE_USER");
+		createPermissionAndRoleIfNotExists("PERMIT_DISABLE_PRODUCT", "ROLE_USER");
+		createPermissionAndRoleIfNotExists("PERMIT_VIEW_PRODUCT", "ROLE_USER");
+		createPermissionAndRoleIfNotExists("PERMIT_MANAGE_ADMIN", "ROLE_ADMIN");
+	}
+
+
+	private void createUsers(){
+		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+		mockRequest.setRemoteAddr("127.0.0.1"); // Simulate IP
+		registerUser("user@gmail.com","#cvdCVD321",mockRequest);
+	}
+
+	private void registerUser(String username,String password,HttpServletRequest httpServletRequest){
+		AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+		authenticationRequest.setUsername(username);
+		authenticationRequest.setPassword(password);
+		authenticationService.register(authenticationRequest,httpServletRequest);
+	}
+
+}
