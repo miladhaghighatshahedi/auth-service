e4207e810c0650656fb6c94b59a01ccb76918a68
Milad <miladhaghighatshahedi@gmail.com>
2025-07-19 14:19:27 +0330

    46th commit: CustomAuthenticationProvider dual invoking solved
    In my previous updates I declared the CustomAuthenticationProvider in the SecurityConfig as an object attribute and used to register it with AuthenticationManagerBuilder.java class and registering the CustomAuthenticationProvider in SecurityConfig.java class as well as setting the CustomAuthenticationProvider as a @Component that implements AuthenticationProvider was the issue which caused Spring security to assume that there are two available AuthenticationProvider in the project.
    
    note: I also remove .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder))) from the SecurityConfig.java config file since the role of this authentication server is to issue tokens, login , logout and register new users.

--- src/main/java/com/mhs/authService/config/SecurityConfig.java
+++ src/main/java/com/mhs/authService/config/SecurityConfig.java
@@ -15,17 +15,15 @@
  */
 package com.mhs.authService.config;
 
-import com.mhs.authService.security.CustomAuthenticationProvider;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -38,8 +36,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @AllArgsConstructor
 public class SecurityConfig {
 
-	private final CustomAuthenticationProvider authenticationProvider;
-	private final JwtDecoder jwtDecoder;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -50,15 +46,13 @@ public class SecurityConfig {
 						.requestMatchers("/auth/register","/auth/login","/auth/rotate","/auth/logout").permitAll()
 						.anyRequest().authenticated()
 				)
-				.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder)))
 				.build();
 	}
 
+
 	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
-		return http.getSharedObject(AuthenticationManagerBuilder.class)
-				.authenticationProvider(authenticationProvider)
-				.build();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 
 }
